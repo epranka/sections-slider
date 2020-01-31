d4cd5d8c87ba75d2960f63cca1d749cea987105a
Edvinas Pranka <epranka@gmail.com>
2020-01-31 15:26:09 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple jQuery sections slider
+
+This is a very simple implementation of the sections slider in jQuery.
+
+Try it live on this [demo](https://epranka.github.io/sections-slider)
+
+Yes, it is very basic. It can be greatly improved, but it just an example how fast it can be implemented. If you need a full-featured slider, use this awesome library [fullpage.js](https://alvarotrigo.com/fullPage/) by [Alvaro Trigo](https://twitter.com/IMAC2)
+
+Follow me on [![](http://i.imgur.com/wWzX9uB.png) Twitter](https://twitter.com/epranka) and [dev.to](https://dev.to/epranka)
+
+![](https://i.ibb.co/wBPqWY3/sections-slider-min.gif)
---
+++ js/main.js
@@ -0,0 +1,73 @@
+(function($) {
+  var selector = ".section";
+
+  var $slides = $(selector);
+
+  var currentSlide = 0;
+  var isAnimating = false;
+
+  var stopAnimation = function() {
+    setTimeout(function() {
+      isAnimating = false;
+    }, 300);
+  };
+
+  var bottomIsReached = function($elem) {
+    var rect = $elem[0].getBoundingClientRect();
+    return rect.bottom <= $(window).height();
+  };
+
+  var topIsReached = function($elem) {
+    var rect = $elem[0].getBoundingClientRect();
+    return rect.top >= 0;
+  };
+
+  document.addEventListener(
+    "mousewheel",
+    function(event) {
+      var $currentSlide = $($slides[currentSlide]);
+
+      if (isAnimating) {
+        event.preventDefault();
+        return;
+      }
+
+      var direction = -event.deltaY;
+
+      if (direction < 0) {
+        // next
+        if (currentSlide + 1 >= $slides.length) return;
+        if (!bottomIsReached($currentSlide)) return;
+        event.preventDefault();
+        currentSlide++;
+        var $slide = $($slides[currentSlide]);
+        var offsetTop = $slide.offset().top;
+        isAnimating = true;
+        $("html, body").animate(
+          {
+            scrollTop: offsetTop
+          },
+          1000,
+          stopAnimation
+        );
+      } else {
+        // back
+        if (currentSlide - 1 < 0) return;
+        if (!topIsReached($currentSlide)) return;
+        event.preventDefault();
+        currentSlide--;
+        var $slide = $($slides[currentSlide]);
+        var offsetTop = $slide.offset().top;
+        isAnimating = true;
+        $("html, body").animate(
+          {
+            scrollTop: offsetTop
+          },
+          1000,
+          stopAnimation
+        );
+      }
+    },
+    { passive: false }
+  );
+})(jQuery);
